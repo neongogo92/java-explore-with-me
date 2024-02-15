6a2f788b67a38cae62f93eeafe901e349525ad9b
neongogo92 <anton.neongogo@yandex.ru>
2024-02-16 02:43:56 +0300

    Добавление комментирования

--- ewm-service/src/main/java/ru/practicum/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.exception;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(Class<?> entityClass, String message) {
-        super("Entity " + entityClass.getSimpleName() + " not found. " + message);
-    }
-}
--- stats-service/server/src/main/java/ru/practicum/exception/StatsValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.exception;
-
-public class StatsValidationException extends RuntimeException {
-    public StatsValidationException(String message) {
-        super(message);
-    }
-}
